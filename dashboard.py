3b446740f39ec9312f0d08faf247093d4258dd10
Nilkamal21 <nilkamaladhikari2005@gmail.com>
2025-07-17 14:15:31 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -162,4 +162,4 @@ def update_graph(dropdown_ticker, input_ticker, interval, period):
 
 # === Run Server ===
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
